498d1f326cf2b3bb16f0298169d99e4d75c4e823
mrsilmar <mrsilmar@gmail.com>
2017-09-14 21:16:20 +0700

    Added some dummy sprites for develop. Added example of console output for Necorsis.

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        //Console output in Unity3D call through 
+        Debug.Log("Sup friends");
+        float a = 0.1f;
+        int b = 2;
+        Debug.LogFormat("Sup friends. Lets count {0} + {1} = {2}", a,b,a+b);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
