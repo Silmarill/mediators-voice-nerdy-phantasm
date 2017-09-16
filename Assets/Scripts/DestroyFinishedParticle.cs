1af1edadd1e173abc0fc50f2c68766fc53df466b
mrsilmar <mrsilmar@gmail.com>
2017-09-17 02:14:54 +0700

    +Системы частиц +Включения-выключения копонентов +Создание и уничтожение объектов +Коорутины и задержки

---
+++ Assets/Scripts/DestroyFinishedParticle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFinishedParticle : MonoBehaviour {
+
+    private ParticleSystem  _ps;
+
+    
+    void Start () {
+        _ps = GetComponent <ParticleSystem>();
+    }
+    
+    
+    void Update () {
+        if (_ps.isPlaying) return;
+        // Уничтожение и создание объекта - это аллоакция памяти. Их лучше избегать.
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,14 +6,19 @@ public class LevelManager : MonoBehaviour {
 
     // Если мы хотим оставить поле для доступа из других классов
     // но убрать из инспектора используем - [HideInInspector] 
-    [HideInInspector]
-    public GameObject currentCheckpoint;
+    [HideInInspector] public GameObject currentCheckpoint;
 
     private PlayerController player;
 
-    
+    // Работа с системой чстиц - занятие творческое. Просто откройте компонент и поиграйте
+    // параметрами для лучшего визуального эффекта.
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
 
-    void Start () {
+    public float respawnDelay;
+
+
+    void Start() {
         // TODO: Если будет мультиплеер - тут будет затык
         player = FindObjectOfType <PlayerController>();
     }
@@ -21,12 +26,37 @@ public class LevelManager : MonoBehaviour {
 
 
     public void RespawnPlayer() {
-        Debug.Log("DEATH");
-        // .transform делает GetComponent<Transform> - это тоже тяжелая операция и по уму трнасформ
-        // надо тоже хешировать. Но тут подразумевается разовое использование, поэтому пойдет.
-        // А вот за такой код в Update() нужно уже наказывать. Жестоко наказывать.
+    //Коорутина - statemashine. Она запускает отдельный поток. Как только доходит до yield
+    // скрипты продолжают работу с предыдущей точки вызова коорутины. Коорутина продолжит работу
+    // параллельно.
+
+        //Допустимо передавать строкой. Но это может привести к проблемам при рефакторинге.
+        StartCoroutine(RespawnPlayerCoorutine());
+    }
+
+
+
+    public IEnumerator RespawnPlayerCoorutine() {
+        Instantiate(deathParticle, player.transform.position, Quaternion.identity);
+
+        //TODO: Подумать, можно ли просто вырубить его через SetActive
+        //вырубаем контроль над персонажем во время задержки
+        player.enabled = false;
+        //выключаем визуальную часть игрока
+        player.GetComponent <SpriteRenderer>().enabled = false;
+
+
+        //Вся корутина затевалась ради ОждиданияНесколькихСекунд
+        yield return new WaitForSeconds(respawnDelay);
 
         // Присваиваем Игроку значение позиции Чекпоинта
         player.transform.position = currentCheckpoint.transform.position;
+
+        //возвращаем всё в зад
+        player.enabled = true;
+        player.GetComponent <SpriteRenderer>().enabled = true;
+
+        //TODO: Нужно переделать инстанцирование на смену позиции объекта+запуск партикл-эффекта (Play)
+        Instantiate(respawnParticle, currentCheckpoint.transform.position, Quaternion.identity);
     }
 }
