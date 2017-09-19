ca7247cce9103e4b4c960e48edeb6d27d8979758
mrsilmar <mrsilmar@gmail.com>
2017-09-20 00:14:28 +0700

    + Simple level select (on S button in ExitZone)

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    private bool isPlayerInZone;
+    public string levelToLoad;
+
+    // Use this for initialization
+    void Start () {
+        isPlayerInZone = false;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.S) && isPlayerInZone) {
+            //Method to switch scenes
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.name == "MY_HERO") {
+            isPlayerInZone = true;
+        }
+    }
+
+     void OnTriggerExit2D(Collider2D other) {
+        if (other.name == "MY_HERO") {
+            isPlayerInZone = true;
+        }
+    }
+}
