13dc279c730a8f4e372c3ae88f0d9d35f75e24e7
mrsilmar <mrsilmar@gmail.com>
2017-09-20 17:50:54 +0700

    + Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public SceneAsset   levelSelect;
+    public SceneAsset   mainMenu;
+
+    public bool isPaused;
+    public GameObject pauseMenuCanvas;
+
+
+    void Update() {
+        if (isPaused) {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0;
+        } else {
+            pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            isPaused = !isPaused;
+        }
+    }
+
+    public void ResumeGame() {
+        isPaused = false;
+    }
+
+    public void LevelSelect() {
+        SceneManager.LoadScene(levelSelect.name);
+    }
+
+    public void QuitToMain() {
+          SceneManager.LoadScene(mainMenu.name);
+    }
+}
