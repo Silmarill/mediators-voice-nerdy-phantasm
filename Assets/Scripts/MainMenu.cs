6c96b8eaf50fe23f60b2b462cf2388df4e3d85a7
mrsilmar <mrsilmar@gmail.com>
2017-09-20 17:12:49 +0700

    + MainMenu Level added + Coin now rotate

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public SceneAsset   startLevel;
+    public SceneAsset   levelSelect;
+
+    public void NewGame() {
+        SceneManager.LoadScene(startLevel.name);
+    }
+
+    public void LevelSelect() {
+        SceneManager.LoadScene(levelSelect.name);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+
+
+}
