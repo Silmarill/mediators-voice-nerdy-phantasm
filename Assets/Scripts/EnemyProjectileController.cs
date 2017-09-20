80abdd85f54e421bad6ae2c9076f5482b484bdc1
mrsilmar <mrsilmar@gmail.com>
2017-09-20 22:33:53 +0700

    +Enemies shooting in range facing to player

---
+++ Assets/Scripts/EnemyProjectileController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectileController : MonoBehaviour {
+
+    
+    public int damageToGive;
+    public float speed;
+    public float rotationSpeed;
+    public GameObject impactEffect;
+
+    private Transform player;
+    private Rigidbody2D _r2d;
+    
+    
+    // Use this for initialization
+    void Start() {
+        _r2d = GetComponent <Rigidbody2D>();
+        player = FindObjectOfType <PlayerController>().GetComponent<Transform>();
+
+        if (player.position.x < GetComponent<Transform>().position.x) {
+            speed = -speed;
+            rotationSpeed = -rotationSpeed;
+        }
+
+        // dont need to keep it in update
+        _r2d.velocity = new Vector2(speed, _r2d.velocity.y);
+        _r2d.angularVelocity = rotationSpeed;
+    }
+
+    
+
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+           HealthManager.HurtPlayer(damageToGive);
+        }
+
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ShootAtPlayerInRange.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAtPlayerInRange : MonoBehaviour {
+
+    public float playerRange;
+    public GameObject enemyStar;
+    private Transform player;
+    public Transform launchPoint;
+
+    public float waitBetweenShoots;
+    private float shootsCounter;
+
+    private Transform _tr;
+    // Use this for initialization
+    void Start () {
+        player = FindObjectOfType <PlayerController>().GetComponent<Transform>();
+        _tr = GetComponent <Transform>();
+        shootsCounter = waitBetweenShoots;
+
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+
+        Debug.DrawLine(new Vector3(_tr.position.x - playerRange, _tr.position.y, _tr.position.z),
+                       new Vector3(_tr.position.x + playerRange, _tr.position.y, _tr.position.z));
+
+        shootsCounter -= Time.deltaTime;
+        if (shootsCounter < 0) {
+            
+            //if enemy  move RIGHT, faced to player and in range
+            if (_tr.localScale.x < 0 && player.position.x > _tr.position.x &&
+                player.position.x < _tr.position.x + playerRange) {
+                Instantiate(enemyStar, launchPoint.position, launchPoint.rotation);
+            }
+
+
+            if (_tr.localScale.x > 0 && player.position.x < _tr.position.x &&
+                player.position.x > _tr.position.x - playerRange) {
+                Instantiate(enemyStar, launchPoint.position, launchPoint.rotation);
+            }
+
+            shootsCounter = waitBetweenShoots;
+        }
+
+
+    }
+}
