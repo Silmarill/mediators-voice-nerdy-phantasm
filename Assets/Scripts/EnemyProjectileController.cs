ad4453abebf6c5c80477cebff42f4e437e741349
mrsilmar <mrsilmar@gmail.com>
2017-09-30 19:01:10 +0700

    +Bullets moving by DOTween +Player's bullets cant hit player

--- Assets/Scripts/EnemyProjectileController.cs
+++ Assets/Scripts/EnemyProjectileController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class EnemyProjectileController : MonoBehaviour {
@@ -19,83 +20,94 @@ public class EnemyProjectileController : MonoBehaviour {
     private bool isPaused;
     private Vector2 velocityBeforePause;
 
+
+    public float travelDuration;
+    public Vector3 moveDistance;
+    private Vector3 moveDistanceStorage;
+
+    public float rotationAmount;
+    private  float rotationAmountStorage;
+
+    private Sequence s;
+
+    void Awake() {
+        player = FindObjectOfType <PlayerController>().GetComponent <Transform>();   
+        _tr = GetComponent <Transform>();
+
+        speedOnStart = speed;
+        moveDistanceStorage = moveDistance;
+        rotationAmountStorage = rotationAmount;
+    }
+
+
+
     void OnEnable() {
         speedOnStart = speed;
-
-        if (_tr == null) {
-            _tr = GetComponent <Transform>();
-        }
-
-
-        if (_r2d == null) {
-            _r2d = GetComponent <Rigidbody2D>();
-        }
-
-        if (player == null) {
-            player = PlayerController.me.gameObject.GetComponent <Transform>();
-        }
-
         
         if (player.position.x < _tr.position.x) {
             speed = -speed;
-            rotationSpeed = -rotationSpeed;
+            rotationAmount = -rotationAmount;
+            moveDistance = -moveDistance;
         }
 
-        // dont need to keep it in update
-        _r2d.velocity = new Vector2(speed, _r2d.velocity.y);
-        _r2d.angularVelocity = rotationSpeed;
-    }
-
-
-
-    // Use this for initialization
-    void Start() {
-        // Добавление слушателя из-за нужды в переопределении логики
-        Messenger.AddListener<bool>("PauseStatus", pauseStatus);
-
-        _r2d = GetComponent <Rigidbody2D>();
-        player = PlayerController.me.gameObject.GetComponent<Transform>();
-        _tr = GetComponent <Transform>();
+      s = DOTween.Sequence();
         
+        //add new animation to sequence 
+        s.Append(_tr.DOBlendableMoveBy(moveDistance, travelDuration)
+                    .OnComplete(gameObject.Recycle)
+                    .SetEase(Ease.Linear));
+        //join() will play WITH curret animation. Not After.
+        s.Join(_tr.DORotate(new Vector3(0, 0, rotationAmount), travelDuration, RotateMode.FastBeyond360)
+                  .SetEase(Ease.Linear));
     }
+
+
+
+    void Start() {
+        Messenger.AddListener<bool>("PauseStatus", PauseStatus);
+    }
+
+
 
     // Получение от слушателя информации о паузе, остановка движения буллетсов
-    void pauseStatus(bool isPaused) {
+    void PauseStatus(bool isPaused) {
         this.isPaused = isPaused;
+
         if (isPaused) {
-            velocityBeforePause = _r2d.velocity;
-            _r2d.velocity = Vector3.zero;
-            _r2d.angularVelocity = 0.0f;
+            _tr.DOPause();
+        } else {
+            _tr.DOPlay();
         }
-        // В случае отжатия паузы всем буллетсам возвращаются их параметры для перемещения
-        else
-        {
-            _r2d.velocity = velocityBeforePause;
-            _r2d.angularVelocity = rotationSpeed;
-        }
-        
+
     }
 
-    //При разрушении объекта убираем слушатель.
-    private void OnDestroy()
-    {
-        Messenger.RemoveListener<bool>("PauseStatus", pauseStatus);
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.tag == "Player") {
+            HealthManager.HurtPlayer(damageToGive);
+        }
+        impactEffect.Spawn(_tr.position, _tr.rotation);
+        gameObject.Recycle();
     }
+
+
 
     void OnDisable() {
         speed = speedOnStart;
-    }
-    
+        moveDistance = moveDistanceStorage;
+        rotationAmount = rotationAmountStorage;
 
-    
-    void OnTriggerEnter2D(Collider2D other) {
-        
-            if (other.tag == "Player")
-            {
-                HealthManager.HurtPlayer(damageToGive);
-            }
-            impactEffect.Spawn(transform.position, transform.rotation);
-            gameObject.Recycle();
-        
+        s.Kill();
     }
+
+
+
+    private void OnDestroy() {
+        Messenger.RemoveListener <bool>("PauseStatus", PauseStatus);
+    }
+
+
+
 }
--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -16,7 +16,7 @@ public class ProjectileController : MonoBehaviour {
     //TODO: Move this property to enemy
     //public int pointsForKill;
 
-    public float rotationSpeed;
+    
     public int damageToGive;
 
     private bool isPaused;
@@ -26,135 +26,98 @@ public class ProjectileController : MonoBehaviour {
     private Transform _tr;
 
 
+    public float travelDuration;
     public Vector3 moveDistance;
     private Vector3 moveDistanceStorage;
-    public float moveDuration;
-    public float rotateDuration;
+
+    public float rotationAmount;
+    private  float rotationAmountStorage;
+    
     private Sequence s;
 
     void Awake() {
-      //  _r2d = GetComponent <Rigidbody2D>();
         player = FindObjectOfType <PlayerController>().GetComponent <Transform>();   // PlayerController.me.gameObject
         _tr = GetComponent <Transform>();
 
-        
-
-
-    }
-    // Use this for initialization
-    void OnEnable() {
         speedOnStart = speed;
         moveDistanceStorage = moveDistance;
+        rotationAmountStorage = rotationAmount;
+    }
 
-        /*
-        if (_r2d == null) {
-              _r2d = GetComponent <Rigidbody2D>();
-        }
 
-        if (player == null) {
-           player = PlayerController.me.gameObject.GetComponent <Transform>();
-        }*/
 
+    // Use this for initialization
+    void OnEnable() {
         if (player.localScale.x < 0) {
-            rotationSpeed = -rotationSpeed;
+            rotationAmount = -rotationAmount;
             speed = -speed;
             moveDistance = -moveDistance;
         }
-
-
-        
-
-        // dont need to keep it in update
-        //_r2d.velocity = new Vector2(speed, _r2d.velocity.y);
-        // _r2d.angularVelocity = rotationSpeed;
-        
-        _tr.DOBlendableMoveBy(moveDistance, moveDuration).SetEase(Ease.Linear).OnComplete(gameObject.Recycle);
-
-        //t.DOBlendableRotateBy(new Vector3(0, 0, rotationSpeed),rotateDuration).SetLoops(-1,LoopType.Incremental);
-
-        
-        /*
+  
         s = DOTween.Sequence();
-
-        s.Append(_tr.DOBlendableMoveBy(moveDistance, moveDuration)
+        
+        //add new animation to sequence 
+        s.Append(_tr.DOBlendableMoveBy(moveDistance, travelDuration)
                     .OnComplete(gameObject.Recycle)
                     .SetEase(Ease.Linear));
-
-        s.Join(_tr.DOBlendableRotateBy(new Vector3(0, 0, rotationSpeed), rotateDuration)
-                  .SetLoops(-1)
+        //join() will play WITH curret animation. Not After.
+        s.Join(_tr.DORotate(new Vector3(0, 0, rotationAmount), travelDuration, RotateMode.FastBeyond360)
                   .SetEase(Ease.Linear));
-                  */
-        //http://easings.net/ru
-
-
-
-    }
-
-    // Получение от слушателя информации о паузе, остановка движения буллетсов
-    void pauseStatus(bool isPaused)
-    {
-        this.isPaused = isPaused;
-
-
-        if (isPaused) {
-            _tr.DOPause();
-            //velocityBeforePause = _r2d.velocity;
-            // _r2d.velocity = Vector3.zero;
-            //  _r2d.angularVelocity = 0.0f;
-        }
-        // В случае отжатия паузы всем буллетсам возвращаются их параметры для перемещения
-        else
-        {
-           _tr.DOPlay();
-            //_r2d.velocity = velocityBeforePause;
-            //_r2d.angularVelocity = rotationSpeed;
-        }
-
-    }
-
-    //При разрушении объекта убираем слушатель.
-    private void OnDestroy()
-    {
-        Messenger.RemoveListener<bool>("PauseStatus", pauseStatus);
-    }
-
-    void OnDisable() {
-        speed = speedOnStart;
-        moveDistance = moveDistanceStorage;
-        _tr.DOKill();
-       // s.Kill();
     }
 
 
 
     void Start() {
-        // Добавление слушателя из-за нужды в переопределении логики
-        Messenger.AddListener<bool>("PauseStatus", pauseStatus);
+        Messenger.AddListener<bool>("PauseStatus", PauseStatus);
+    }
 
-        //_r2d = GetComponent <Rigidbody2D>();
-        // player = PlayerController.me.gameObject.GetComponent <Transform>();
 
-        //_tr = GetComponent <Transform>();
-        //_tr.DOM
+
+    // Получение от слушателя информации о паузе, остановка движения пуль
+    void PauseStatus(bool isPaused){
+        this.isPaused = isPaused;
+
+        if (isPaused) {
+            _tr.DOPause();
+        } else {
+            _tr.DOPlay();
+        }
     }
 
 
 
     void OnTriggerEnter2D(Collider2D other) {
-      
+
         if (other.tag == "Enemy") {
             other.GetComponent <EnemyHealthManager>().GiveDamage(damageToGive);
         }
 
         if (other.tag == "Boss") {
-            other.GetComponent<BossHealthManager>().GiveDamage(damageToGive);
+            other.GetComponent <BossHealthManager>().GiveDamage(damageToGive);
         }
 
-        impactEffect.Spawn(transform.position, transform.rotation);
-
-        //_tr.DOKill();
+        impactEffect.Spawn(_tr.position, _tr.rotation);
         gameObject.Recycle();
-
     }
+
+
+
+    void OnDisable() {
+        speed = speedOnStart;
+        moveDistance = moveDistanceStorage;
+        rotationAmount = rotationAmountStorage;
+
+        s.Kill();
+    }
+
+    
+    //При разрушении объекта убираем слушатель.
+    private void OnDestroy(){
+        Messenger.RemoveListener<bool>("PauseStatus", PauseStatus);
+    }
+    
+
+
+    
 
 }
