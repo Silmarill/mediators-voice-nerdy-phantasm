52c7edb5c1090cd38dbadd1b3c888017bf1ef4d2
Belobopka <belobaka1@gmail.com>
2017-10-10 23:07:45 +0300

    Fixed bug with 2 or more thiefs.Changed some logic (lost 2-d attack tho).

--- Assets/Scripts/Enemy_Thief/ThiefAttackChecker.cs
+++ Assets/Scripts/Enemy_Thief/ThiefAttackChecker.cs
@@ -3,17 +3,25 @@ using System.Collections;
 
 public class ThiefAttackChecker : MonoBehaviour {
     private Animator _ator;
+    public EnemyThiefController _etc;
     void Start() {
+     //   _etc = FindObjectOfType<EnemyThiefController>();
         _ator = GetComponentInParent<Animator>();
     }
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag != "Player") return;  
         _ator.SetBool("isAttacking", true);      
     }
-    private void OnTriggerStay2D(Collider2D collision) {
-            if (collision.tag != "Player") return;
-            _ator.SetBool("isAttacking", true);
+   /* private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.tag != "Player") return;
+        
+        _ator.SetBool("isAttacking", true);
+     }
+   */ 
+   
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag != "Player") return;
+        _etc.DeActivateTrigger();
         
     }
-    
 }
